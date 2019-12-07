ac32fd310141258988bff9a2d82ae8d14a17ce03
Julia <midnightwhisper@web.de>
2019-12-07 11:02:51 +0100

    Create lexicon.lang.php

---
+++ inc/languages/deutsch_du/lexicon.lang.php
@@ -0,0 +1,18 @@
+<?php
+
+// Navigation
+$l['lexicon'] = "Lexikon";
+$l['lexicon_list'] = "Liste";
+$l['lexicon_view_entry'] = "Eintrag ansehen";
+$l['lexicon_as'] = "wie... [Einträge]";
+
+$l['lexicon_start'] = "<strong>Herzlich Willkommen</strong> im foreninternen Lexikon! Hier finden Sie alle Informationen, die Sie brauchen, um an Board mitwirken zu können. Die <em>Suche</em> befindet sich auf jeder Seite im Headerbereich der Tabelle; Sie können nach Stichwort oder Kategorie filtern. In der Navigation links können Sie das Lexikon nach dem Alphabet geordnet durchlesen - und auch die einzelnen Buchstaben noch einmal nach dem gesuchten Begriff filtern. <br /><br />
+Ihnen fehlt eine Information? Dann wenden Sie sich an ein <a href=\"showteam.php\">Teammitglied</a>, damit Ihr Wunsch besprochen und ggf. umgesetzt werden kann.";
+
+
+$l['lexicon_search'] = "Eintrag suchen";
+$l['lexicon_search_word'] = "Suchwort eingeben";
+
+$l['lexicon_none'] = "Für diese Suche sind keine Einträge vorhanden.";
+$l['lexicon_select_cat'] = "Kategorie auswählen";
+?>
