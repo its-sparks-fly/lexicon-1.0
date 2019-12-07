b2e0a7f3350a27801c58bf253f7d2d2ee2274d0f
Julia <midnightwhisper@web.de>
2019-12-07 11:03:35 +0100

    Update lexicon.lang.php

--- inc/languages/deutsch_du/lexicon.lang.php
+++ inc/languages/deutsch_du/lexicon.lang.php
@@ -6,8 +6,8 @@ $l['lexicon_list'] = "Liste";
 $l['lexicon_view_entry'] = "Eintrag ansehen";
 $l['lexicon_as'] = "wie... [Einträge]";
 
-$l['lexicon_start'] = "<strong>Herzlich Willkommen</strong> im foreninternen Lexikon! Hier finden Sie alle Informationen, die Sie brauchen, um an Board mitwirken zu können. Die <em>Suche</em> befindet sich auf jeder Seite im Headerbereich der Tabelle; Sie können nach Stichwort oder Kategorie filtern. In der Navigation links können Sie das Lexikon nach dem Alphabet geordnet durchlesen - und auch die einzelnen Buchstaben noch einmal nach dem gesuchten Begriff filtern. <br /><br />
-Ihnen fehlt eine Information? Dann wenden Sie sich an ein <a href=\"showteam.php\">Teammitglied</a>, damit Ihr Wunsch besprochen und ggf. umgesetzt werden kann.";
+$l['lexicon_start'] = "<strong>Herzlich Willkommen</strong> im foreninternen Lexikon! Hier findest du alle Informationen, die du brauchst, um an Board mitwirken zu können. Die <em>Suche</em> befindet sich auf jeder Seite im Headerbereich der Tabelle; du kannst nach Stichwort oder Kategorie filtern. In der Navigation links kannst du das Lexikon nach dem Alphabet geordnet durchlesen - und auch die einzelnen Buchstaben noch einmal nach deinem gesuchten Begriff filtern. <br /><br />
+Dir fehlt eine Information? Dann wende dich doch an ein <a href=\"showteam.php\">Teammitglied</a>, damit dein Wunsch besprochen und ggf. umgesetzt werden kann.";
 
 
 $l['lexicon_search'] = "Eintrag suchen";
