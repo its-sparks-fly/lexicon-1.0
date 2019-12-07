61c6edb691e6e8e49c6e9e5cb1674095d3181a4d
Julia <midnightwhisper@web.de>
2019-12-07 11:01:26 +0100

    Create lexicon.lang.php

---
+++ inc/languages/deutsch_sie/admin/lexicon.lang.php
@@ -0,0 +1,41 @@
+<?php
+
+$l['lexicon_name'] = "Lexikon";
+$l['lexicon_short_desc'] = "Erweitert MyBB um ein alphabetisches Lexikon.";
+
+$l['lexicon_permissions'] = "Kann das Lexikon verwalten?";
+
+$l['lexicon_manage'] = "Lexikon verwalten";
+$l['lexicon_manage_overview_entries'] = "Einträge";
+$l['lexicon_manage_overview_entries_desc'] = "Eine Übersicht aller vorhandenen Lexikon-Einträge.";
+$l['lexicon_manage_edit_entry'] = "Einträge bearbeiten";
+$l['lexicon_manage_edit_cat'] = "Kategorien bearbeiten";
+$l['lexicon_manage_overview_cats'] = "Kategorien";
+$l['lexicon_manage_overview_cats_desc'] = "Eine Übersicht aller vorhandenen Lexikon-Kategorien.";
+$l['lexicon_manage_add_entry'] = "Eintrag hinzufügen";
+$l['lexicon_manage_add_entry_desc'] = "Einen neuen Eintrag in das Lexikon hinzufügen.";
+$l['lexicon_manage_add_cat'] = "Kategorie hinzufügen";
+$l['lexicon_manage_add_cat_desc'] = "Eine neue Kategorie ins Lexikon hinzufügen.";
+
+$l['lexicon_manage_title'] = "Name";
+$l['lexicon_manage_content'] = "Inhalt";
+$l['lexicon_manage_cat'] = "Kategorie";
+$l['lexicon_manage_options'] = "Optionen";
+$l['lexicon_manage_edit'] = "Bearbeiten";
+$l['lexicon_manage_delete'] = "Löschen";
+$l['lexicon_manage_submit'] = "Absenden";
+
+$l['lexicon_manage_cat_name_desc'] = "Wie lautet der Name der Kategorie?";
+$l['lexicon_manage_entry_name_desc'] = "Welches Thema wird in dem Eintrag behandelt?";
+$l['lexicon_manage_entry_title_desc'] = "Fügen Sie hier den Inhalt ein. HTML und BBCode werden angezeigt.";
+
+$l['lexicon_manage_cat_added'] = "Eine neue Kategorie wurde hinzugefügt ";
+$l['lexicon_manage_entry_added'] = "Ein neuer Eintrag wurde hinzugefügt ";
+$l['lexicon_manage_cat_edited'] = "Die Kategorie wurde bearbeitet ";
+$l['lexicon_manage_entry_edited'] = "Der Eintrag wurde bearbeitet ";
+$l['lexicon_manage_cat_deleted'] = "Die Kategorie wurde gelöscht ";
+$l['lexicon_manage_entry_deleted'] = "Der Eintrag wurde gelöscht ";
+$l['lexicon_manage_error_no_title'] = "Es muss ein Name angegeben werden.";
+$l['lexicon_manage_error_no_text'] = "Es muss ein Inhalt angegeben werden.";
+$l['lexicon_manage_error_invalid'] = "Ungültige Aktion";
+?>
