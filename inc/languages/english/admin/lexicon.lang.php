8bd65be3988df2e216f259ca2873955c15a37d2c
Sven <SvePu@users.noreply.github.com>
2023-02-17 15:03:28 +0100

    english lang files

---
+++ inc/languages/english/admin/lexicon.lang.php
@@ -0,0 +1,43 @@
+<?php
+
+$l['lexicon_name'] = "Lexicon";
+$l['lexicon_short_desc'] = "Extends MyBB with an alphabetical lexicon.";
+
+$l['lexicon_uninstall'] = "Lexicon - Uninstallation";
+$l['lexicon_uninstall_message'] = "Do you wish to drop all plugin entries from the database? - This cannot be undone!";
+
+$l['lexicon_permissions'] = "Can manage the lexicon?";
+
+$l['lexicon_manage'] = "Manage Lexicon";
+$l['lexicon_manage_overview_entries'] = "Entries";
+$l['lexicon_manage_overview_entries_desc'] = "An overview of all existing lexicon entries.";
+$l['lexicon_manage_edit_entry'] = "Edit Entry";
+$l['lexicon_manage_edit_cat'] = "Edit category";
+$l['lexicon_manage_overview_cats'] = "Categories";
+$l['lexicon_manage_overview_cats_desc'] = "An overview of all existing lexicon categories.";
+$l['lexicon_manage_add_entry'] = "Add Entry";
+$l['lexicon_manage_add_entry_desc'] = "Add a new entry to the lexicon.";
+$l['lexicon_manage_add_cat'] = "Add Category";
+$l['lexicon_manage_add_cat_desc'] = "Add a new category to the lexicon.";
+
+$l['lexicon_manage_title'] = "Name";
+$l['lexicon_manage_content'] = "Content";
+$l['lexicon_manage_cat'] = "Category";
+$l['lexicon_manage_options'] = "Options";
+$l['lexicon_manage_edit'] = "Edit";
+$l['lexicon_manage_delete'] = "Delete";
+$l['lexicon_manage_submit'] = "Submit";
+
+$l['lexicon_manage_cat_name_desc'] = "What is the name of the category?";
+$l['lexicon_manage_entry_name_desc'] = "What topic is covered in the entry?";
+$l['lexicon_manage_entry_title_desc'] = "Paste the content here. HTML and BBCode are displayed.";
+
+$l['lexicon_manage_cat_added'] = "A new category has been added ";
+$l['lexicon_manage_entry_added'] = "A new entry has been added ";
+$l['lexicon_manage_cat_edited'] = "Die Kategorie wurde bearbeitet ";
+$l['lexicon_manage_entry_edited'] = "The category has been edited ";
+$l['lexicon_manage_cat_deleted'] = "The category has been deleted ";
+$l['lexicon_manage_entry_deleted'] = "The entry has been deleted ";
+$l['lexicon_manage_error_no_title'] = "A name must be specified.";
+$l['lexicon_manage_error_no_text'] = "Content must be specified.";
+$l['lexicon_manage_error_invalid'] = "Invalid action";
---
+++ inc/languages/english/lexicon.lang.php
@@ -0,0 +1,15 @@
+<?php
+
+// Navigation
+$l['lexicon'] = "Lexicon";
+$l['lexicon_list'] = "List";
+$l['lexicon_view_entry'] = "View Entry";
+$l['lexicon_all'] = "All Entries";
+$l['lexicon_as'] = "{1} like... [Entries]";
+$l['lexicon_like'] = " ( {1} like... )";
+$l['lexicon_start'] = "<strong>Welcome</strong> to the internal forum lexicon! Here you will find all the information you need to get involved on board. The <em>search</em> is on every page in the header area of the table; You can filter by keyword or category. In the navigation on the left you can read through the lexicon in alphabetical order and you can also filter the individual letters for the term you are looking for. <br /><br />
+Are you missing information? Then contact a <a href=\"showteam.php\">team member</a> so that your request can be discussed and, if necessary, implemented.";
+$l['lexicon_search'] = "Search entries";
+$l['lexicon_search_word'] = "Enter keyword";
+$l['lexicon_none'] = "There are no entries for this search.";
+$l['lexicon_select_cat'] = "Select category";
